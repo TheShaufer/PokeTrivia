e60b587c54e8cb2a3a3b0688eb8f29ba38280264
TheShaufer <geoffreychenard@gmail.com>
2025-04-18 04:23:54 -0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,218 @@
+const questions = {
+  "0-0": {
+    question: "<img src='emoji/100/comb.png'> <img src='emoji/100/bee.png'>",
+    answer: "<div class='text'>Answer: COMBEE</div><img src='emoji/100/combee.png'>",
+    image: null,
+    video: null
+  },
+  "1-0": {
+    question: "<img src='emoji/200/poly.png'> <img src='emoji/200/toad.png'>",
+    answer: "<div class='text'>Answer: POLITOED</div><img src='emoji/200/politoed.png'>",
+    image: null,
+    video: null
+  },
+  "2-0": {
+    question: "<img src='emoji/300/tent.png'> <img src='emoji/300/cool.png'>",
+    answer: "<div class='text'>Answer: TENTACOOL</div><img src='emoji/300/tentacool.png'>",
+    image: null,
+    video: null
+  },
+  "3-0": {
+    question: "<img src='emoji/400/go.png'> <img src='emoji/400/goat.png'>",
+    answer: "<div class='text'>Answer: GOGOAT</div><img src='emoji/400/gogoat.png'>",
+    image: null,
+    video: null
+  },
+  "4-0": {
+    question: "<img src='emoji/500/duster.png'> <img src='emoji/500/ox.png'>",
+    answer: "<div class='text'>Answer: DUSTOX</div><img src='emoji/500/dustox.png'>",
+    image: null,
+    video: null
+  },
+  "5-0": {
+    question: "<img src='emoji/600/eyes.png'> <img src='emoji/600/buck.png'>",
+    answer: "<div class='text'>Answer: SAWSBUCK</div><img src='emoji/600/sawsbuck.png'>",
+    image: null,
+    video: null
+  },
+  "0-1": {
+    question: "<img src='Jifs/100/Wooper.gif'>",
+    answer: "<div class='text'>Answer: WOOPER</div><img src='Jifs/100/wooper.png'>",
+    image: null,
+    video: null
+  },
+  "1-1": {
+    question: "<img src='Jifs/200/drifloon.gif'>",
+    answer: "<div class='text'>Answer: DRIFLOON</div><img src='Jifs/200/drifloon.png'>",
+    image: null,
+    video: null
+  },
+  "2-1": {
+    question: "<img src='Jifs/300/litten.gif'>",
+    answer: "<div class='text'>Answer: LITTEN</div><img src='Jifs/300/litten.png'>",
+    image: null,
+    video: null
+  },
+  "3-1": {
+    question: "<img src='Jifs/400/flygon.gif'>",
+    answer: "<div class='text'>Answer: FLYGON</div><img src='Jifs/400/flygon.png'>",
+    image: null,
+    video: null
+  },
+  "4-1": {
+    question: "<img src='Jifs/500/cherrim.gif'>",
+    answer: "<div class='text'>Answer: CHERRIM</div><img src='Jifs/500/cherrim.png'>",
+    image: null,
+    video: null
+  },
+  "5-1": {
+    question: "<img src='Jifs/600/magcargo.gif'>",
+    answer: "<div class='text'>Answer: MAGCARGO</div><img src='Jifs/600/magcargo.png'>",
+    image: null,
+    video: null
+  },
+  "0-2": {
+    question: "<img src='Gen 1/100/ninetales.jpg'>",
+    answer: "<div class='text'>Answer: NINETALES</div><img src='Gen 1/100/ninetales.png'>",
+    image: null,
+    video: null
+  },
+  "1-2": {
+    question: "<img src='Gen 1/200/arcanine.jpg'>",
+    answer: "<div class='text'>Answer: ARCANINE</div><img src='Gen 1/200/arcanine.png'>",
+    image: null,
+    video: null
+  },
+  "2-2": {
+    question: "<img src='Gen 1/300/mewtwo1.png'>",
+    answer: "<div class='text'>Answer: MEWTWO</div><img src='Gen 1/300/mewtwo.png'>",
+    image: null,
+    video: null
+  },
+  "3-2": {
+    question: "<img src='Gen 1/400/paras1.png'>",
+    answer: "<div class='text'>Answer: PARAS</div><img src='Gen 1/400/paras.png'>",
+    image: null,
+    video: null
+  },
+  "4-2": {
+    question: "<img src='Gen 1/500/machoke1.png'>",
+    answer: "<div class='text'>Answer: MACHOKE</div><img src='Gen 1/500/machoke.png'>",
+    image: null,
+    video: null
+  },
+  "5-2": {
+    question: "<img src='Gen 1/600/lapras.jpg'>",
+    answer: "<div class='text'>Answer: LAPRAS</div><img src='Gen 1/600/lapras.png'>",
+    image: null,
+    video: null
+  },
+  "0-3": {
+    question: "<div class='text'>When the moon hits your eye Like a big pizza pie, that's</div>",
+    answer: "<div class='text'>Answer: AMAURA</div><img src='questions/100/amaura.png'>",
+    image: null,
+    video: null
+  },
+  "1-3": {
+    question: "<div class='text'>What was the primary cause of the fall of the Western Roman Empire in the late 5th century CE?</div>",
+    answer: "<div class='text'>Answer: NIDOKING</div><img src='questions/200/nidoking.png'>",
+    image: null,
+    video: null
+  },
+  "2-3": {
+    question: "<div class='text'>I'm friends with my Korean car, they're my?</div>",
+    answer: "<div class='text'>Answer: PALKIA</div><img src='questions/300/palkia.png'>",
+    image: null,
+    video: null
+  },
+  "3-3": {
+    question: "<div class='text'>Golly gee! My food isn't cooked enough I wonder what I should do?</div>",
+    answer: "<div class='text'>Answer: HEATMOR</div><img src='questions/400/heatmor.png'>",
+    image: null,
+    video: null
+  },
+  "4-3": {
+    question: "<div class='text'>What is the airspeed velocity of an unladen swallow?</div>",
+    answer: "<div class='text'>Answer: AUDINO</div><img src='questions/500/audino.png'>",
+    image: null,
+    video: null
+  },
+  "5-3": {
+    question: "<div class='text'>When the last ember choked on ash and even the wind forgot its name, he stayed behind — not out of duty, but because he could not run from what he’d done. The silence didn’t forgive, and the darkness did not judge; it simply remained. No light could reach the place where he knelt, buried beneath the weight of every choice that led him there. And when the silence finally broke, it came not with fire, but with the voice of despair itself — not mourning what was lost, but what was done.</div>",
+    answer: "<div class='text'>Answer: DARKRAI</div><img src='questions/600/darkrai.png'>",
+    image: null,
+    video: null
+  },
+  "0-4": {
+    question: "<div class='text'>Name a pokemon that has another pokemon in it's art.</div>",
+    answer: "<div class='text'>Answer: MANTINE, CRAMORANT</div><img src='nerd/100/mantine.png'><img src='nerd/100/cramorant.png'>",
+    image: null,
+    video: null
+  },
+  "1-4": {
+    question: "<div class='text'>What is the heaviest non-legendary pokemon?</div>",
+    answer: "<div class='text'>Answer: CELESTEELA</div><img src='nerd/200/celesteela.png'>",
+    image: null,
+    video: null
+  },
+  "2-4": {
+    question: "<div class='text'>Name all Pikachu clone lines.</div>",
+    answer: "<div class='text'>Answer: PIKACHU, PLUSLE, MINUN, PACHIRISU, EMOLGA, DEDENNE, TOGEDEMARU, MORPEKO, PAWMI, MIMIKYU</div><img src='nerd/300/pika.png'>",
+    image: null,
+    video: null
+  },
+  "3-4": {
+    question: "<div class='text'>What was the one way you able to teach vaporeon mist is pokemon yellow?</div>",
+    answer: "<div class='text'>Answer: LEVEL UP IN DAYCARE</div><img src='nerd/400/vaporeon.png'>",
+    image: null,
+    video: null
+  },
+  "4-4": {
+    question: "<div class='text'>Name a pokemon that has this type interaction</div><img src='nerd/500/chart.png'>",
+    answer: "<div class='text'>Answer: LILEEP,CRADILY, and, OGERPON,</div><img src='nerd/500/lileep.png'><img src='nerd/500/cradily.png'><img src='nerd/500/ogerpon.png'>",
+    image: null,
+    video: null
+  },
+  "5-4": {
+    question: "<div class='text'>Which type from generation 1 has one 1 pokemon representing it?</div>",
+    answer: "<div class='text'>Answer: BIRD TYPE (MISSINGNO)</div><img src='nerd/600/missingno.png'>",
+    image: null,
+    video: null
+  },
+  "0-5": {
+    question: "<img src='puns/100/houndour1.png'>",
+    answer: "<div class='text'>Answer: HOUNDOUR</div><img src='puns/100/houndour.png'>",
+    image: null,
+    video: null
+  },
+  "1-5": {
+    question: "<img src='puns/200/piloswine1.png'>",
+    answer: "<div class='text'>Answer: PILOSWINE</div><img src='puns/200/piloswine.png'>",
+    image: null,
+    video: null
+  },
+  "2-5": {
+    question: "<img src='puns/300/rhyhorn.jpg'>",
+    answer: "<div class='text'>Answer: RHYHORN</div><img src='puns/300/rhyhorn.png'>",
+    image: null,
+    video: null
+  },
+  "3-5": {
+    question: "<img src='puns/400/seaking.jpg'>",
+    answer: "<div class='text'>Answer: SEAKING</div><img src='puns/400/seaking.png'>",
+    image: null,
+    video: null
+  },
+  "4-5": {
+    question: "<img src='puns/500/whimsicott1.png'>",
+    answer: "<div class='text'>Answer: WHIMSICOTT</div><img src='puns/500/whimsicott.png'>",
+    image: null,
+    video: null
+  },
+  "5-5": {
+    question: "<img src='puns/600/miraidon1.png'>",
+    answer: "<div class='text'>Answer: MIRAIDON</div><img src='puns/600/miraidon.png'>",
+    image: null,
+    video: null
+  }
+};
